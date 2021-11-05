4c3d2fbd35068822542c1d1fe48b105dcacd54eb
vis <vedantbhopatrao15@gmail.com>
2021-11-05 12:40:11 +0000

    Update particle.py

--- Game Code/particle.py
+++ Game Code/particle.py
@@ -1,4 +1,15 @@
 import pygame
 
 class Particle:
-    def __init__(self, color, x, y, size, mass)
+    def __init__(self, color, x, y, size, mass):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.size = size
+        self.mass = mass
+
+    def move(self):
+        pass
+
+    def bounce(self):
+        pass
