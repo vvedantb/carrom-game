1ce0cd511465dfb3ad1d080f0cd958cbe2ebe886
vis <vedantbhopatrao15@gmail.com>
2021-11-12 12:28:18 +0000

    Insert photoshop made board

--- Game Code/main.py
+++ Game Code/main.py
@@ -37,7 +37,7 @@ BLUE = (0, 0, 255)
 DARK_GREY = (125, 125, 125)
 LIGHT_BROWN = (191, 134, 0)
 
-Board = p.image.load("Game Code/board.jpg")
+Board = p.image.load("Game Code/board.png")
 
 
 screen = p.display.set_mode((WIDTH, HEIGHT), p.RESIZABLE) # Sets the display resolution
@@ -54,12 +54,6 @@ FPS = 60 # Sets the refresh rate, i.e. 60 times a second
 mouse_click = False # If a click has been made by the mouse
 
 screen.fill(BROWN)
-
-
-
-
-def collide(p1, p2):
-    pass
 
 
 def event_manager():
@@ -115,6 +109,16 @@ def mainMenu():
         )
         options_button.draw(screen, BLACK, 30, CRIMSON)
 
+        settings_button = Button(
+            color=WHITE,
+            x=30,
+            y=570,
+            width=500,
+            height=50,
+            text="Settings"
+        )
+        settings_button.draw(screen, BLACK, 30, CRIMSON)
+
         quit_button = Button(
             color=WHITE,
             x=630,
@@ -151,6 +155,9 @@ def mainMenu():
         if options_button.mouse_collide() and mouse_click == True:
             options()
 
+        if settings_button.mouse_collide() and mouse_click == True:
+            settings()
+
         if quit_button.mouse_collide() and mouse_click == True:
             quit_game()
 
@@ -159,74 +166,6 @@ def mainMenu():
 
         p.display.flip() # Updates display
         clock.tick(FPS) 
-
-
-
-# def mainMenu():     
-#     while True:
-#         screen.fill(BROWN)
-#         mouse_click = False # If a click has been made by the mouse
-
-#         drawText("Main Menu", font, WHITE, screen, 20, 20)
-
-#         button_height = screen.get_height()/20
-#         button_width = screen.get_width()/10
-
-#         game_button = p.Rect(button_width-20, (WIDTH/10)+5, button_width, button_height)
-#         options_button = p.Rect(button_width-20, (2*WIDTH/10)+5, button_width, button_height)
-#         settings_button = p.Rect(button_width-20, (3*WIDTH/10)+5, button_width, button_height)
-#         quit_button = p.Rect(button_width-20, (4*WIDTH/10)+5, button_width/2, button_height)
-#         music_button = p.Rect(button_width-20, (5*WIDTH/10)+5, button_width, button_height)
-
-      
-#         p.draw.rect(screen, BLACK, game_button) # Draws a rectangle onto the screen in white colour for button 1
-#         p.draw.rect(screen, BLACK, options_button)
-#         p.draw.rect(screen, BLACK, settings_button)
-#         p.draw.rect(screen, BLACK, quit_button)
-#         p.draw.rect(screen, BLACK, music_button)
-
-#         text_width = screen.get_width()/10
-#         text_height = screen.get_height()/6
-
-#         drawText("Play Game", font, WHITE, screen, text_height, 1.1*text_width) # Writes text onto the screen with the specified font at the start with a black colour onto the screen
-#         drawText("Options", font, WHITE, screen, text_height, 2.1*text_width)
-#         drawText("Settings", font, WHITE, screen, text_height, 3.1*text_width)
-#         drawText("Quit", font, WHITE, screen, text_height, 4.1*text_width)
-#         drawText("Music", font, WHITE, screen, text_height, 5.1*text_width)
-
-#         events = p.event.get()
-#         for event in events:
-#             if event.type == QUIT: # Checks if the cross (top right button) has been pressed
-#                 p.quit() # Program ends
-#                 sys.exit()
-#             if event.type == KEYDOWN: # Checks if a key has been pressed
-#                 if event.key == K_ESCAPE: # Checks if escape key has been pressed
-#                     p.quit() # Programs ends
-#                     sys.exit()
-#             if event.type == MOUSEBUTTONDOWN: # Checks if mouse button has been pressed
-#                 if event.button == 1: # Checks if button has been clicked
-#                     mouse_click = True
-
-#         mx, my = p.mouse.get_pos() # Retrieves the current position of where the mouse is
-
-#         if game_button.collidepoint((mx, my)): # Checks if mouse location overlaps button 1 location
-#             if mouse_click: # Checks if button has been clicked
-#                 game()
-#         if options_button.collidepoint((mx, my)):
-#             if mouse_click:
-#                 options()
-#         if settings_button.collidepoint((mx, my)): 
-#             if mouse_click:
-#                 settings()
-#         if quit_button.collidepoint((mx, my)): 
-#             if mouse_click:
-#                 quit_game()
-#         if music_button.collidepoint((mx, my)):
-#             if mouse_click:
-#                 play_music(button_width, button_height, text_height, text_width)
-
-#         p.display.flip() # Updates display
-#         clock.tick(FPS) 
 
 
 def draw_pieces():
@@ -282,13 +221,13 @@ def game():
     while run:
         screen.fill(BROWN)
         BOARD_WIDTH, BOARD_HEIGHT = 900, 900
-        BG = p.transform.rotate(p.transform.scale(p.image.load(os.path.join('Game Code\Assets', 'carrom-board.jpg')), ((int(screen.get_width()*0.5625)), screen.get_height())), 89.5) # TODO: Change this line
+        BG = p.transform.scale(p.image.load("Game Code/board.png"), ((int(screen.get_width()*0.5625)), screen.get_height())) # TODO: Change this line
         screen.blit(BG, (int(screen.get_width()/2 - (BOARD_WIDTH/2)), 0)) # Puts board in the middle
 
         drawText("Play Game", font, BLACK, screen, 20, 20) # Writes text onto the screen 
 
-        player_striker = Striker.draw_circle(screen, RED, (BOARD_WIDTH/2, BOARD_HEIGHT/2), 15)
-        enemy_striker = Striker.draw_circle(screen, BLUE, (BOARD_WIDTH/2 + 15, BOARD_HEIGHT/2), 15)
+        # player_striker = Striker.draw_circle(screen, RED, (BOARD_WIDTH/2, BOARD_HEIGHT/2), 15)
+        # enemy_striker = Striker.draw_circle(screen, BLUE, (BOARD_WIDTH/2 + 15, BOARD_HEIGHT/2), 15)
 
         events = p.event.get()
         for event in events:
@@ -306,27 +245,27 @@ def game():
         WIDTH = screen.get_width()
         HEIGHT = screen.get_height()
 
-        draw_pieces()
+        # draw_pieces()
 
 
-        p.draw.circle(screen, RED, (2*WIDTH/6, 2*HEIGHT/8), 15) 
-        p.draw.circle(screen, RED, (2*WIDTH/6, 6*HEIGHT/8), 15) 
+        # p.draw.circle(screen, RED, (2*WIDTH/6, 2*HEIGHT/8), 15) 
+        # p.draw.circle(screen, RED, (2*WIDTH/6, 6*HEIGHT/8), 15) 
 
-        p.draw.circle(screen, RED, (4*WIDTH/6, 2*HEIGHT/8), 15) 
-        p.draw.circle(screen, RED, (4*WIDTH/6, 6*HEIGHT/8), 15)
+        # p.draw.circle(screen, RED, (4*WIDTH/6, 2*HEIGHT/8), 15) 
+        # p.draw.circle(screen, RED, (4*WIDTH/6, 6*HEIGHT/8), 15)
 
-        p.draw.circle(screen, RED, (4*WIDTH/6 - 30, 6*HEIGHT/8 + 50), 15) 
-        p.draw.circle(screen, RED, (2*WIDTH/6 + 30, 6*HEIGHT/8 + 50), 15) 
+        # p.draw.circle(screen, RED, (4*WIDTH/6 - 30, 6*HEIGHT/8 + 50), 15) 
+        # p.draw.circle(screen, RED, (2*WIDTH/6 + 30, 6*HEIGHT/8 + 50), 15) 
 
-        p.draw.circle(screen, RED, (4*WIDTH/6 - 30, 2*HEIGHT/8 - 50), 15) 
-        p.draw.circle(screen, RED, (2*WIDTH/6 + 30, 2*HEIGHT/8 - 50), 15) # Striker boundaries
+        # p.draw.circle(screen, RED, (4*WIDTH/6 - 30, 2*HEIGHT/8 - 50), 15) 
+        # p.draw.circle(screen, RED, (2*WIDTH/6 + 30, 2*HEIGHT/8 - 50), 15) # Striker boundaries
 
-        p.draw.circle(screen, RED, (WIDTH/2, HEIGHT/2), 20) # Center circle
+        # p.draw.circle(screen, RED, (WIDTH/2, HEIGHT/2), 20) # Center circle
 
-        p.draw.circle(screen, DARK_GREY, (WIDTH/4 + 30, HEIGHT/8 - 30), 30) # Top left
-        p.draw.circle(screen, DARK_GREY, (WIDTH/4 + 30, 7*HEIGHT/8 + 30), 30) # Bottom left
-        p.draw.circle(screen, DARK_GREY, (3*WIDTH/4 - 30, HEIGHT/8 - 30), 30) # Top right
-        p.draw.circle(screen, DARK_GREY, (3*WIDTH/4 - 30, 7*HEIGHT/8 + 30), 30) # Bottom right corner holes
+        # p.draw.circle(screen, DARK_GREY, (WIDTH/4 + 30, HEIGHT/8 - 30), 30) # Top left
+        # p.draw.circle(screen, DARK_GREY, (WIDTH/4 + 30, 7*HEIGHT/8 + 30), 30) # Bottom left
+        # p.draw.circle(screen, DARK_GREY, (3*WIDTH/4 - 30, HEIGHT/8 - 30), 30) # Top right
+        # p.draw.circle(screen, DARK_GREY, (3*WIDTH/4 - 30, 7*HEIGHT/8 + 30), 30) # Bottom right corner holes
 
         for i in range(20):
             p.draw.circle(screen, DARK_GREY, (i* WIDTH/100, HEIGHT/2), 10)
@@ -395,7 +334,18 @@ def play_music():
         )
         resume_music_button.draw(screen, BLACK, 30, CRIMSON)
 
-        event_manager()
+        events = p.event.get()
+        for event in events:
+            if event.type == QUIT: # Checks if the cross (top right button) has been pressed
+                p.quit() # Program ends
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    run = False
+            if event.type == MOUSEBUTTONDOWN: # Checks if mouse button has been pressed
+                if event.button == 1: # Checks if button has been clicked
+                    mouse_click = True    
+
 
         if return_button.mouse_collide() and mouse_click == True:
             run = False
@@ -448,17 +398,39 @@ def options():
         test_button.draw(screen, BLACK, 30, CRIMSON)
 
         test2_button = Button(
-            WHITE,
-            30,
-            370,
-            500, 
-            50,
-            "Select Pieces Colour"
+            color=WHITE,
+            x=30,
+            y=370,
+            width=500, 
+            height=50,
+            text="Select Pieces Colour"
         )
 
         test2_button.draw(screen, BLACK, 30, CRIMSON)
 
-        event_manager()
+
+        resolution_button = Button(
+            color=WHITE,
+            x=30,
+            y=570,
+            width=500, 
+            height=50,
+            text="Change resolution"
+        )
+        resolution_button.draw(screen, BLACK, 30, CRIMSON)
+
+        events = p.event.get()
+        for event in events:
+            if event.type == QUIT: # Checks if the cross (top right button) has been pressed
+                p.quit() # Program ends
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    run = False
+            if event.type == MOUSEBUTTONDOWN: # Checks if mouse button has been pressed
+                if event.button == 1: # Checks if button has been clicked
+                    mouse_click = True    
+
 
         if test_button.mouse_collide() and mouse_click == True:
             print("It works!")
@@ -471,9 +443,100 @@ def options():
         if return_button.mouse_collide() and mouse_click == True:
             mainMenu()
 
+        if resolution_button.mouse_collide() and mouse_click == True:
+            resolution_settings()
+
         p.display.update()
         clock.tick(FPS)
 
+
+
+def resolution_settings():
+    run = True
+    screen = p.display.set_mode((1600, 900))
+    while True:
+        screen.fill(BROWN)
+        mouse_click = False
+
+        res1_button = Button(
+            color=WHITE,
+            x=30,
+            y=170,
+            width=500,
+            height=50,
+            text="1280x720"
+        )
+        res1_button.draw(screen, WHITE, 30, CRIMSON)
+
+        res2_button = Button(
+            color=WHITE,
+            x=30,
+            y=370,
+            width=500,
+            height=50,
+            text="1600x900"
+        )
+        res2_button.draw(screen, WHITE, 30, CRIMSON)     
+
+        res3_button = Button(
+            color=WHITE,
+            x=30,
+            y=570,
+            width=500,
+            height=50,
+            text="1920x1080"
+        )
+        res3_button.draw(screen, WHITE, 30, CRIMSON)  
+
+        res4_button = Button(
+            color=WHITE,
+            x=30,
+            y=770,
+            width=500,
+            height=50,
+            text="Apply Fullscreen"
+        )
+        res4_button.draw(screen, WHITE, 30, CRIMSON)                           
+
+        back_button = Button(
+            color=BLACK,
+            x=30,
+            y=970,
+            width=500,
+            height=50,
+            text="Back"
+        )
+        back_button.draw(screen, WHITE, 30, CRIMSON)   
+
+        events = p.event.get()
+        for event in events:
+            if event.type == QUIT:
+                p.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    run = False
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mouse_click = True
+        
+        if res1_button.mouse_collide() and mouse_click == True:
+            WIDTH, HEIGHT = 1280, 720             
+            screen = p.display.set_mode((WIDTH, HEIGHT))
+
+        if res2_button.mouse_collide() and mouse_click == True:
+            WIDTH, HEIGHT = 1600, 900
+            screen = p.display.set_mode((WIDTH, HEIGHT))
+
+        if res3_button.mouse_collide() and mouse_click == True:
+            WIDTH, HEIGHT = 1920, 1080
+            screen = p.display.set_mode((WIDTH, HEIGHT))
+
+        if res4_button.mouse_collide() and mouse_click == True:
+            pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+        if back_button.mouse_collide() and mouse_click == True:
+            run = False
 
 
 
@@ -519,7 +582,18 @@ def settings():
         )
         y_sensitivity_button.draw(screen, text_color=BLACK, text_size=30, outline=CRIMSON)
 
-        event_manager()
+        events = p.event.get()
+        for event in events:
+            if event.type == QUIT: # Checks if the cross (top right button) has been pressed
+                p.quit() # Program ends
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    run = False
+            if event.type == MOUSEBUTTONDOWN: # Checks if mouse button has been pressed
+                if event.button == 1: # Checks if button has been clicked
+                    mouse_click = True    
+
         
         if x_sensitivity_button.mouse_collide() and mouse_click == True:
             print("Omg! It works!")
